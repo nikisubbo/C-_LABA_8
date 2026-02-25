645848dabe0bd95a7ab4fe1f7c7edccab880f1a1
Tel Mithryn <145218442+nikisubbo@users.noreply.github.com>
2026-02-26 00:23:38 +0500

    1.1

--- LABA_9/Recur15.cpp
+++ LABA_9/Recur15.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <string>
-#include "WorkWithFiles.h"
 #include "Recur15.h"
 void Recur15() {
 	std::cout << "Задание Recur15" << std::endl;
-	std::string fike_name = PromptForFileName();
+	std::string s;
+	std::cin.ignore();
+	std::cout << "Введите ваше выражение: ";
+	std::getline(std::cin, s);
+	std::cout << "Ваше выражение: " << s << std::endl;
+	int position = 0;
+	int number = s[position] - '0';
+	position++;
+}
+int Recursive() {
 
 }
